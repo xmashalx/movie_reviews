574619d54dfd06e311d5ccc12ff1f6555fe9dcbf
xmashalx <Mashalchr@gmail.com>
2026-01-18 21:31:41 +0000

    add all changes

--- reviewdb/create_database.sql
+++ reviewdb/create_database.sql
@@ -1,4 +1,4 @@
-DROP DATABASE reviews;
-CREATE DATABASE reviews;
+DROP DATABASE moviereviews;
+CREATE DATABASE moviereviews;
 
 \echo 'CREATED reviewsDB: run the following command -> psql -d reviews -f schema.sql'
